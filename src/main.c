1e6ee5e72999bf5f02fad972cd8b4d66a73d2bca
Jonas Norling <jonas@elemental.se>
2021-07-24 14:02:56 +0200

    Blink red LED

--- src/main.c
+++ src/main.c
@@ -1,9 +1,45 @@
 #include "logging/log.h"
+#include <drivers/gpio.h>
 
 LOG_MODULE_REGISTER(main);
+
+const struct device *l_gpio1 = NULL;
+
+#define GPIO_PIN_LED 13
+
+static int gpio_init()
+{
+    int rc;
+
+    l_gpio1 = device_get_binding(DT_LABEL(DT_NODELABEL(gpio0)));
+    if (!l_gpio1) {
+        LOG_ERR("Failed to open GPIO device");
+        return 1;
+    }
+
+    rc = gpio_pin_configure(l_gpio1, GPIO_PIN_LED, GPIO_OUTPUT_LOW);
+    if (rc) {
+        LOG_ERR("Error configuring pin");
+        return 1;
+    }
+
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
     LOG_INF("Hello there");
+
+    if (gpio_init()) {
+        return 1;
+    }
+
+    while (true) {
+        k_sleep(K_MSEC(100));
+        gpio_pin_set(l_gpio1, GPIO_PIN_LED, 1);
+        k_sleep(K_MSEC(100));
+        gpio_pin_set(l_gpio1, GPIO_PIN_LED, 0);
+    }
+
     return 0;
 }
